3f4c5a2bb3b36e2de3be078de2344462e335d459
brendancjz <brendancjz@gmail.com>
2022-02-20 14:56:15 +0800

    Edit datainit singleton sb

--- Tompang/Tompang-ejb/src/java/ejb/singleton/DataInitSessionBean.java
+++ Tompang/Tompang-ejb/src/java/ejb/singleton/DataInitSessionBean.java
@@ -5,6 +5,7 @@
  */
 package ejb.singleton;
 
+import entity.CreditCard;
 import entity.User;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -28,28 +29,36 @@ public class DataInitSessionBean implements DataInitSessionBeanLocal {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-    
     @PostConstruct
     public void postConstruct() {
         System.out.println("=== DataInit postConstruct() called ====");
-        
+
         if (em.find(User.class, 1L) == null) {
-            
             //Create Manager
             Date dob = Date.from(LocalDate.of(1999, 12, 25).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             Date joinedOn = Date.from(LocalDate.of(2022, 2, 19).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             User manager = new User("Brendan", "Chia", "bchia@gmail.com", "manager", "password", dob, 98769876L, joinedOn, true);
-            
+
             em.persist(manager);
             em.flush();
-            
+
             //Create Dummy User
             dob = Date.from(LocalDate.of(1999, 1, 1).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             joinedOn = Date.from(LocalDate.of(2022, 2, 20).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             User dummyUser = new User("Dummy", "User", "dummyUser@gmail.com", "dummy", "password", dob, 12341234L, joinedOn, false);
-            
+
             em.persist(dummyUser);
             em.flush();
+        }
+
+        if (em.find(CreditCard.class, 1L) == null) {
+            CreditCard cc = new CreditCard("BRENDAN CHIA", 4605123456781010L, 123, "05/25");
+            em.persist(cc);
+            em.flush();
+
+            //Associate CreditCard to User
+            User manager = (User) em.find(User.class, 1L);
+            manager.getCreditCards().add(cc);
         }
     }
 }
