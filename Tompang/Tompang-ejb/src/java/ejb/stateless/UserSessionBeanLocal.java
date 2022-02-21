60cf8f3f0ac3e24982d42be870e701df77215071
brendancjz <brendancjz@gmail.com>
2022-02-21 20:13:54 +0800

    created user session bean

---
+++ Tompang/Tompang-ejb/src/java/ejb/stateless/UserSessionBeanLocal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.stateless;
+
+import entity.Listing;
+import entity.User;
+import exception.EmptyListException;
+import exception.EntityNotFoundException;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author brend
+ */
+@Local
+public interface UserSessionBeanLocal {
+
+    public Long createNewUser(User user);
+
+    public List<User> retrieveAllUsers() throws EmptyListException;
+
+    public User getUserByUserId(Long userId) throws EntityNotFoundException;
+
+    public void updateUserDetails(Long userId, String firstName, String lastName, String email, String username, Date dob, Long contactNum) throws EntityNotFoundException;
+
+    public void updateUserPassword(Long userId, String password) throws EntityNotFoundException;
+
+    public void associateListingToUser(Listing listing, Long userId) throws EntityNotFoundException;
+    
+}
