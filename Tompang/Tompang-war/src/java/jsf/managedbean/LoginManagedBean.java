604b26fa2a6b1ca34045ab1de8bc5580c79e5384
brendancjz <brendancjz@gmail.com>
2022-02-25 00:30:18 +0800

    completed the user login features and styling

--- Tompang/Tompang-war/src/java/jsf/managedbean/LoginManagedBean.java
+++ Tompang/Tompang-war/src/java/jsf/managedbean/LoginManagedBean.java
@@ -5,11 +5,16 @@
  */
 package jsf.managedbean;
 
+import ejb.stateless.UserSessionBeanLocal;
+import entity.User;
+import exception.EntityNotFoundException;
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,14 +24,61 @@ import javax.faces.event.ActionEvent;
 @RequestScoped
 public class LoginManagedBean {
 
-    /**
-     * Creates a new instance of LoginManagedBean
-     */
+    @EJB
+    private UserSessionBeanLocal userSessionBean;
+    private String username;
+    private String password;
+
     public LoginManagedBean() {
     }
-    
+
     public void redirectToLoginPage(ActionEvent event) throws IOException {
-        System.out.println(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath());
         FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/login.xhtml");
     }
+
+    public void redirectToIndexPage(ActionEvent event) throws IOException {
+        System.out.println("Code comes here.");
+        FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+    }
+    
+    public void login(ActionEvent event) throws IOException {
+        try {
+            User user = (User) userSessionBean.retrieveUserByUsername(username);
+
+            if (user.getPassword().equals(password)) {
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isLogin", true);
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("currentUser", user);
+                FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+            }
+
+        } catch (EntityNotFoundException ex) {
+            System.out.println("No User found.");
+        }
+    }
+
+    public void logout(ActionEvent event) throws IOException {
+        ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true)).invalidate();
+        FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+    }
+
+    public void back(ActionEvent event) throws IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect(FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/index.xhtml");
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
