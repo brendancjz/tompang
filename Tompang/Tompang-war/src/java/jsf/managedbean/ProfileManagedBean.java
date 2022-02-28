2832ec721d5c0eff56ff750842d8bdd70711fcd6
brendancjz <brendancjz@gmail.com>
2022-02-28 13:07:14 +0800

    updated profile page, not completed. Also updated Home page

--- Tompang/Tompang-war/src/java/jsf/managedbean/ProfileManagedBean.java
+++ Tompang/Tompang-war/src/java/jsf/managedbean/ProfileManagedBean.java
@@ -5,9 +5,13 @@
  */
 package jsf.managedbean;
 
+import ejb.stateless.UserSessionBeanLocal;
 import entity.User;
+import java.io.Serializable;
+import java.util.Date;
+import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.faces.view.ViewScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -15,12 +19,31 @@ import javax.faces.context.FacesContext;
  * @author brend
  */
 @Named(value = "profileManagedBean")
-@Dependent
-public class ProfileManagedBean {
+@ViewScoped
+public class ProfileManagedBean implements Serializable {
+
+    @EJB
+    private UserSessionBeanLocal userSessionBean;
 
     private User user;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Long contactNum;
+    private Date dob;
+    private Date joinedOn;
+    
+    
     public ProfileManagedBean() {
         user = (User) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("currentUser");
+        username = user.getUsername();
+        email = user.getEmail();
+        firstName = user.getFirstName();
+        lastName = user.getLastName();
+        contactNum = user.getContactNumber();
+        dob = user.getDateOfBirth();
+        joinedOn = user.getJoinedOn();
     }
 
     public User getUser() {
@@ -30,5 +53,63 @@ public class ProfileManagedBean {
     public void setUser(User user) {
         this.user = user;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getContactNum() {
+        return contactNum;
+    }
+
+    public void setContactNum(Long contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Date getJoinedOn() {
+        return joinedOn;
+    }
+
+    public void setJoinedOn(Date joinedOn) {
+        this.joinedOn = joinedOn;
+    }
+    
+    
     
 }
