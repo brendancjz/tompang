3a4b1446dbe6984848461c8a4b8dfb3663c5b910
Guo Jun <hengguojun25@gmail.com>
2022-03-02 02:27:28 +0800

    Added View Listings and Listing Details managed beans and xhtml pages

---
+++ Tompang/Tompang-war/src/java/jsf/managedbean/ViewAllListingsManagedBean.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import ejb.stateless.ListingSessionBeanLocal;
+import entity.Listing;
+import exception.EmptyListException;
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author GuoJun
+ */
+@Named(value = "viewAllListingsManagedBean")
+@RequestScoped
+public class ViewAllListingsManagedBean {
+
+    @EJB
+    private ListingSessionBeanLocal listingSessionBean;
+    
+    private List<Listing> listings;
+    
+    public ViewAllListingsManagedBean() {
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        try {
+            listings = listingSessionBean.retrieveAllListings();
+        } catch (EmptyListException ex) {
+            System.out.println("List of listings empty.");
+        }
+    }
+    
+    public void viewListingDetails(ActionEvent event) throws IOException {
+        Long listingIdToView = (Long)event.getComponent().getAttributes().get("listingId");
+        System.err.print(listingIdToView);
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("listingIdToView", listingIdToView);
+        FacesContext.getCurrentInstance().getExternalContext().redirect("viewListingDetails.xhtml");
+    }
+    
+    public List<Listing> getListings() {
+        return listings;
+    }
+
+    public void setListOfListings(List<Listing> listings) {
+        this.listings = listings;
+    }
+    
+}
