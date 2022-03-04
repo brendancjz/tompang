aa74e08d0dbbc62b11ff68f86c1824ba23550584
ignitiusgohyz <ignitiusgohyz@gmail.com>
2022-03-05 05:38:58 +0800

    not sure why user is still showing up after deletion..

--- Tompang/Tompang-war/src/java/jsf/managedbean/ViewAllUsersManagedBean.java
+++ Tompang/Tompang-war/src/java/jsf/managedbean/ViewAllUsersManagedBean.java
@@ -8,6 +8,7 @@ package jsf.managedbean;
 import ejb.stateless.UserSessionBeanLocal;
 import entity.User;
 import exception.EmptyListException;
+import exception.EntityNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,8 +17,10 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 
 /**
@@ -53,6 +56,24 @@ public class ViewAllUsersManagedBean {
         Long userIdToView = (Long) event.getComponent().getAttributes().get("userId");
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("userIdToView", userIdToView);
         FacesContext.getCurrentInstance().getExternalContext().redirect("viewUserDetails.xhtml");
+    }
+
+    public void deleteUser(ActionEvent event) {
+        try {
+            User userEntityToDelete = (User) event.getComponent().getAttributes().get("userEntityToDelete");
+            userSessionBean.deleteUser(userEntityToDelete.getUserId());
+
+            listOfUsers.remove(userEntityToDelete);
+
+            if (filteredUsers != null) {
+                filteredUsers.remove(userEntityToDelete);
+            }
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "User deleted successfully", null));
+        } catch (EntityNotFoundException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An error has occurred while deleting user: " + ex.getMessage(), null));
+        } catch (Exception ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An unexpected error has occurred: " + ex.getMessage(), null));
+        }
     }
 
     public ViewUserDetailsManagedBean getViewUserDetailsManagedBean() {
