dab0615c97230be5a805d92c87aa4d7221ad9991
brendancjz <brendancjz@gmail.com>
2022-03-07 15:01:28 +0800

    adding user pages for admin to run user use cases

--- Tompang/Tompang-war/src/java/jsf/managedbean/ViewAllUsersManagedBean.java
+++ Tompang/Tompang-war/src/java/jsf/managedbean/ViewAllUsersManagedBean.java
@@ -10,15 +10,12 @@ import entity.User;
 import exception.EmptyListException;
 import exception.EntityNotFoundException;
 import java.io.IOException;
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -30,8 +27,8 @@ import javax.inject.Inject;
  * @author brend
  */
 @Named(value = "viewAllUsersManagedBean")
-@RequestScoped
-public class ViewAllUsersManagedBean {
+@ViewScoped
+public class ViewAllUsersManagedBean implements Serializable {
 
     @EJB
     private UserSessionBeanLocal userSessionBean;
