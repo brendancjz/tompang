fb69d7746f9f186a766bc2739f7e17694b0b8505
brendancjz <brendancjz@gmail.com>
2022-03-28 12:38:57 +0800

    adding on to Index page

--- tompang-app/src/app/index/index.page.ts
+++ tompang-app/src/app/index/index.page.ts
@@ -15,13 +15,15 @@ export class IndexPage implements OnInit {
   username: string | undefined;
   password: string | undefined;
   loginError: boolean;
+  showLoginBox: boolean;
 
   constructor(private router: Router,
     private activatedRoute: ActivatedRoute,
-    private sessionService: SessionService,
+    public sessionService: SessionService,
     private userService: UserService) { }
 
   ngOnInit() {
+    this.showLoginBox = false;
   }
 
   userLogin(): void {
@@ -32,13 +34,28 @@ export class IndexPage implements OnInit {
       this.sessionService.setIsLogin(true);
       this.sessionService.setCurrentUser(user);
       this.loginError = false;
+      this.showLoginBox = false; //Hide the login box.
+      this.username = null;
+      this.password = null;
 
-      this.router.navigate(['/shop']);
+      this.router.navigate(['/shop']); //After successful login, direct to Shop
     }
     else
     {
       this.loginError = true;
     }
+  }
+
+  userLogout(): void {
+    this.sessionService.setIsLogin(false);
+    this.sessionService.setCurrentUser(null);
+
+    this.router.navigate(['/index']);
+  }
+
+  displayLoginBox(): void {
+    this.showLoginBox = true;
+
   }
 
 }
--- tompang-app/src/app/shop/shop.page.ts
+++ tompang-app/src/app/shop/shop.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { SessionService } from '../services/session.service';
+
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public sessionService: SessionService) { }
 
   ngOnInit() {
+    this.checkUserLogin();
   }
 
+
+  checkUserLogin(): void {
+    console.log('Checking User Login Status..');
+    if (!this.sessionService.getIsLogin()) {
+      this.router.navigate(['/index']);
+    }
+  }
 }
