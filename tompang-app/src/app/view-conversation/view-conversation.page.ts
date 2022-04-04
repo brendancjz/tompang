02be1821d2843dafc4b17f9a041ed307e15fc144
brendancjz <brendancjz@gmail.com>
2022-04-04 10:40:15 +0800

    more changes to the view conversation

--- tompang-app/src/app/view-conversation/view-conversation.page.ts
+++ tompang-app/src/app/view-conversation/view-conversation.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Conversation } from '../models/conversation';
+import { User } from '../models/user';
 import { ConversationService } from '../services/conversation.service';
 import { ListingService } from '../services/listing.service';
 import { SessionService } from '../services/session.service';
@@ -18,6 +19,9 @@ export class ViewConversationPage implements OnInit {
 
   hasLoaded: boolean;
 
+  currentUser: User | null;
+  currentUserIsTalkingTo: User | null;
+
   constructor(private router: Router,
     private activatedRoute: ActivatedRoute,
     public sessionService: SessionService,
@@ -26,6 +30,7 @@ export class ViewConversationPage implements OnInit {
 
   ngOnInit() {
     this.convoId = this.activatedRoute.snapshot.paramMap.get('convoId');
+    this.currentUser = this.sessionService.getCurrentUser();
 
     if(this.convoId != null)
     {
@@ -36,7 +41,24 @@ export class ViewConversationPage implements OnInit {
       // eslint-disable-next-line radix
       this.convoToView = this.conversationService.getConversationById(parseInt(this.convoId));
 
+      if (this.convoToView.createdBy === this.currentUser) {
+        console.log('Current user created this convo');
+        this.currentUserIsTalkingTo = this.convoToView.listing.createdBy;
+      } else {
+        console.log('Current user is the seller of this listing');
+        this.currentUserIsTalkingTo = this.convoToView.createdBy;
+      }
     }
   }
 
+  getConversationListingUrl(): string {
+    const basePictureUrl = '../../assets/images';
+
+    return basePictureUrl + this.convoToView.listing.photos[0];
+  }
+
+  viewListing(): void {
+    console.log('View listing details from convo page');
+    this.router.navigate(['/view-listing-details/' + this.convoToView.listing.listingId]);
+  }
 }
