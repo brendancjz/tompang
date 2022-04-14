586808901d0c316de7bd6f3c13de4989ed0f9c8d
seanangwj <78225993+seanangwj@users.noreply.github.com>
2022-04-14 18:51:06 +0800

    ok

--- Tompang/Tompang-war/src/java/jsf/managedbean/ViewUserDetailsManagedBean.java
+++ Tompang/Tompang-war/src/java/jsf/managedbean/ViewUserDetailsManagedBean.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 
@@ -24,6 +25,15 @@ public class ViewUserDetailsManagedBean implements Serializable {
         String username = (String)event.getComponent().getAttributes().get("username");
         FacesContext.getCurrentInstance().getExternalContext().getFlash().put("username", username);
         FacesContext.getCurrentInstance().getExternalContext().redirect("viewAllListings.xhtml");
+    }
+    
+    public void viewUserProfile(AjaxBehaviorEvent event) throws IOException {
+        System.out.println("*** UserProfileManagedBean.viewUserProfile()");
+        User user = (User) event.getComponent().getAttributes().get("user");
+        System.out.print(user.getUsername());
+        
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userToView", user);
+        FacesContext.getCurrentInstance().getExternalContext().redirect("../userPages/viewUserProfile.xhtml");
     }
 
     public User getUserEntityToView() {
